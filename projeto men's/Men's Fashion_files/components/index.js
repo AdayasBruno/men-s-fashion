05b945b9a3c047e83d7528f3d9ae6bb91ed9086c
AdayasBruno <adayasbruno7@gmail.com>
2025-05-06 20:23:30 -0300

    deletou o arquivo

--- projeto men's/Men's Fashion_files/components/index.js
+++
@@ -1,7 +0,0 @@
- function Teste() {
-     console.log("clicou")
-     const texto = document.getElementById("texto")
-     texto.textContent = ' <h2 id="result">resultado</h2>'
-     const result = document.getElementById("result")
-     result.textContent = (2 * 50).toString()
- }
